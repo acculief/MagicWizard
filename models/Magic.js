8f5178813423ad424bbd7a347ac0f5a15cd35247
YujiroKusano <yujiro.kusano.1106@gmail.com>
2021-02-09 16:23:55 +0900

    [mod] 文字による魔法の生成処理を追加

--- models/Magic.js
+++ models/Magic.js
@@ -1,70 +1,111 @@
-import magicJson from "../json/magic.json";
+import magic from '../json/magic.json';
 class Magic {
-	constructor(word, role) {
-		return this._create(word, role);
-	}
+  constructor(word, role) {
+    return this._create(word, role);
+  }
 
-	/**
-     * 魔法生成
-     * 
-     * @param {String} word 魔法の呪文 
-     * @param {String} role 攻撃: 'attacker' 防御: 'blocker' 
-     * @returns {String, Integer, String} magic
-     */
-	_create(word, role) {
-		// 後々wordによって魔法を分けるためランダム魔法をsampleとする
-		const randomMagic = magicJson.random;
-		let count = this._createCount(randomMagic);
-		let type = this._createType(randomMagic, role);
-		return {word, count, type};
+  /**
+   * 魔法生成
+   *
+   * @param {String} word 魔法の呪文
+   * @param {String} role 攻撃: 'attacker' 防御: 'blocker'
+   * @returns {String, Integer, String} magic
+   */
+  _create(word='', role) {
+    // typeを算出
+    let type = this._generateType(word, role);
+    // attributeを算出
+    let attribute = this._generateAttribute(word, type);
+    // countを算出
+    let count = this._generateCount(word, attribute);
+    let status = {
+      word: word,
+      count:
+      attribute.default_count +
+        Math.floor(Math.random() * (count.max + 1 - count.min) + count.min),
+      type: type.name,
+    };
+    return status;
+  }
 
-	}
+  _generateType(word='', role) {
+    // word判定
+    for (let type in magic) {
+      if (this._searchArrayWord(word, magic[type].word) !== -1 && magic[type].role === role) {
+        return magic[type];
+      }
+    }
+    // ランダム判定
+    let probability = 0;
+    let maxPercent = 0;
+    for (let type in magic) {
+      if (magic[type].role === role) {
+        maxPercent += magic[type].probability;
+      }
+    }
+    let random = Math.random() * (maxPercent + 1);
+    for (let type in magic) {
+      if (magic[type].role === role) {
+        probability += magic[type].probability;
+        if (random <= probability) {
+          return magic[type];
+        }
+      }
+    }
+  }
 
-	/**
-	 * 魔法の威力を算出
-	 * 
-	 * @param {*} magic 魔法の基礎値 
-	 */
-	_createCount(magic) {
-		const DEFAULT_DAMAGE = magic.default_count;
-		let count = magic.count
-		let probability = 0;
-		let maxPercent = 0;
-		// 確率の合計値を算出
-		for (let key in count) {
-			maxPercent += count[key].probability;
-		}
-		let random = Math.random() * (maxPercent + 1);
-		for (let key in count) {
-			probability += count[key].probability;
-			if (random <= probability) {
-				return DEFAULT_DAMAGE +
-					Math.floor(Math.random() * (count[key].max + 1 - count[key].min) + count[key].min);
-			}
-		}
-	}
-	/**
-	 * 魔法の種類を算出
-	 * 
-	 * @param {*} magic 魔法の基礎値
-	 * @param {*} role 攻撃: 'attacker' 防御: 'blocker' 
-	 */
-	_createType(magic, role) {
-		let type = magic.role[role].type;
-		let probability = 0;
-		let maxPercent = 0;
-		// 確率の合計値を算出
-		for (let key in type) {
-			maxPercent += type[key].probability;
-		}
-		let random = Math.random() * (maxPercent + 1);
-		for (let key in type) {
-			probability += type[key].probability;
-			if (random <= probability) {
-				return type[key].name;
-			}
-		}
-	}
+  _generateAttribute(word='', type) {
+    // word判定
+    for (let attribute in type.attribute) {
+      if (this._searchArrayWord(word, type.attribute[attribute].word) !== -1) {
+        return type.attribute[attribute];
+      }
+    }
+    // ランダム判定
+    let probability = 0;
+    let maxPercent = 0;
+    for (let attribute in type.attribute) {
+      maxPercent += type.attribute[attribute].probability;
+    }
+    let random = Math.random() * (maxPercent + 1);
+    for (let attribute in type.attribute) {
+      probability += type.attribute[attribute].probability;
+      if (random <= probability) {
+        return type.attribute[attribute];
+      }
+    }
+  }
+
+  _generateCount(word='', attribute) {
+    // word
+    for (let index in attribute.count) {
+      if (this._searchArrayWord(word, attribute.count[index].word) !== -1) {
+        return attribute.count[index];
+      }
+    }
+    // ランダム判定
+    let probability = 0;
+    let maxPercent = 0;
+    for (let index in attribute.count) {
+      maxPercent += attribute.count[index].probability;
+    }
+    let random = Math.random() * (maxPercent + 1);
+    for (let index in attribute.count) {
+      probability += attribute.count[index].probability;
+      if (random <= probability) {
+        return attribute.count[index];
+      }
+    }
+  }
+  _searchArrayWord(word='', arrayWord) {
+    for (let target in arrayWord) {
+      let result = word.indexOf(arrayWord[target]);
+      if (result !== -1) {
+        return result;
+      }
+    }
+    return -1;
+  }
 }
 
 export default Magic;
